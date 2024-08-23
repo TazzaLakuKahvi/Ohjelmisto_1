d4e88b4a9f1e4e14008e0a8040d5f985ba09b887
jerep <jerepun@metropolia.fi>
2024-08-23 22:36:24 +0300

    Moduuli 2 valmis

--- Python_Ohjelmistoteema/Tehtavat/Moduuli_2/2_5.py
+++ Python_Ohjelmistoteema/Tehtavat/Moduuli_2/2_5.py
@@ -1,4 +1,5 @@
-print("Tämä ohjelma laskee leivisköjen määrän kilogrammoina ja grammoina.\nAnna leivisköjen määrä, kiitos.")
+print("Tämä ohjelma laskee leivisköjen, naulojen ja luotien massan kilogrammoina ja grammoina.")
+print("Anna mittojen määrä, kiitos.")
 leiviska = input("leivisköjen määrä: ")
 naula = input("naulojen määrä: ")
 luoti = input("luotien määrä: ")
@@ -6,3 +7,6 @@ luodinpaino = float(luoti) * 13.3
 naulanpaino = float(naula) * 13.3 * 32
 leiviskanpaino = float(leiviska) * 13.3 * 32 * 20
 kokonaispaino = luodinpaino + naulanpaino + leiviskanpaino
+kilot = int(kokonaispaino / 1000)
+grammat = kokonaispaino - (kilot * 1000)
+print(f"Massa nykymittojen mukaan: {kilot}kg {grammat:.2f}g")
---
+++ Python_Ohjelmistoteema/Tehtavat/Moduuli_2/2_6.py
@@ -0,0 +1,4 @@
+import random
+print("Tässä numerolukon koodinne, hra Wayne.\nTerveisin, Alfred Pennyworth\nHovimestari")
+print("kolminumeroinen koodi: " + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
+print("nelinumeroinen koodi: " + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)))

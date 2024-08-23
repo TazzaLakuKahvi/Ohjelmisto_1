142db06fb12ca385f03436f1a2b422757f3f54ac
jerep <jerepun@metropolia.fi>
2024-08-23 22:42:06 +0300

    lisä kommentti 2.6 :)

--- Python_Ohjelmistoteema/Tehtavat/Moduuli_2/2_6.py
+++ Python_Ohjelmistoteema/Tehtavat/Moduuli_2/2_6.py
@@ -2,3 +2,4 @@ import random
 print("Tässä numerolukon koodinne, hra Wayne.\nTerveisin, Alfred Pennyworth\nHovimestari")
 print("kolminumeroinen koodi: " + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
 print("nelinumeroinen koodi: " + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)))
+#vähän yanderedev metodilla, mutta se toimii :)

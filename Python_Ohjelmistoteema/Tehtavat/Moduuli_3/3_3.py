7e510bcbbdccf712770226379f1d6e66b452f8b4
jerep <jerepun@metropolia.fi>
2024-08-30 11:16:45 +0300

    tehtävä 3.3 valmis tehtävä 3.4 aloitettu

--- Python_Ohjelmistoteema/Tehtavat/Moduuli_3/3_3.py
+++ Python_Ohjelmistoteema/Tehtavat/Moduuli_3/3_3.py
@@ -1,18 +1,18 @@
 print("Mikä on sinun biologinen sukupuolesi sekä hemoglobiiniarvosi?")
 sukupuoli = input("Sukupuoli: ")
 hemoglobiini = input("Hemoglobiiniarvo (g/l): ")
-if sukupuoli.lower() == "nainen" or "n" or "tyttö":
-    if int(117) <= float(hemoglobiini) <= int(175):
+if sukupuoli.lower() == "nainen" or sukupuoli.lower() == "n" or sukupuoli.lower() == "tyttö":
+    if 117 <= float(hemoglobiini) <= 175:
         print("Hemoglobiini arvosi on normaali.")
-    elif float(hemoglobiini) < float(117):
+    elif float(hemoglobiini) < 117:
         print("Hemoglobiini arvosi on alhainen.")
-    elif float(hemoglobiini) > float(175):
+    elif float(hemoglobiini) > 175:
         print("Hemoglobiini arvosi on korkea.")
-elif sukupuoli.lower() == "mies" or "m" or "poika":
-    if int(134) <= float(hemoglobiini) <= int(195):
+elif sukupuoli.lower() == "mies" or sukupuoli.lower() == "m" or sukupuoli.lower() == "poika":
+    if 134 <= float(hemoglobiini) <= 195:
         print("Hemoglobiini arvosi on normaali.")
-    elif float(hemoglobiini) < float(134):
+    elif float(hemoglobiini) < 134:
         print("Hemoglobiini arvosi on alhainen.")
-    elif float(hemoglobiini) > float(195):
+    elif float(hemoglobiini) > 195:
         print("Hemoglobiini arvosi on korkea.")
 else: print("Antamasi tiedot ovat virheelliset.")

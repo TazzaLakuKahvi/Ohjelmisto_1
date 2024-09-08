6fb7cc0b321805b003a875b96cc08c3df5b9ba5b
jerep <jerepun@metropolia.fi>
2024-09-08 23:51:46 +0300

    tehtävä 6.4 kesken tehtävä 4.6 kesken

---
+++ Python_Ohjelmistoteema/Tehtavat/Moduuli_6/6_4.py
@@ -0,0 +1,9 @@
+def yhteen_lasku(lista):
+
+    return
+lista = []
+luku = input("Anna kokonaisluku: ")
+while luku != "":
+    lista.append(int(luku))
+    luku = input("Anna kokonaisluku: ")
+yhteen_lasku(lista)

586e87003d65d6714ca0970ecd6332b9d272fda7
jerep <jerepun@metropolia.fi>
2024-09-16 12:17:22 +0300

    tehtävä 7.3 valmis tehtävä 4.6 kesken

--- Python_Ohjelmistoteema/Tehtavat/Moduuli_7/7_3.py
+++ Python_Ohjelmistoteema/Tehtavat/Moduuli_7/7_3.py
@@ -1,7 +1,25 @@
 lentoasemat = {}
 uusi_asema = 0
-print("Haluatko lisätä uuden lentokentän(1), hakea jo syötettyä kenttää(2) vai lopettaa ohjelman(3)?")
-paatos = int(input("Valitse yllä olevista vaihtoehdoista painamalla 1, 2 tai 3: "))
 
-if paatos == 1:
-    uusi_asema = input("Anna uusi lentokenttä")
+print("Haluatko lisätä uuden lentokentän(uusi), hakea jo syötettyä kenttää(hae) vai lopettaa ohjelman(lopeta)?")
+paatos_teksti = "Valitse yllä olevista vaihtoehdoista syöttämällä uusi, hae tai lopeta: "
+paatos = input(paatos_teksti)
+paatos = paatos.lower()
+
+while paatos != "lopeta":
+    if paatos == "uusi":
+        uusi_koodi = input("Anna uuden lentokentän ICAO-koodi: ")
+        uusi_koodi = uusi_koodi.upper()
+        uusi_asema = input("Anna uuden lentokentän nimi: ")+" lentokenttä"
+        lentoasemat[uusi_koodi] = uusi_asema
+    elif paatos == "hae":
+        haku = input("Anna haettavan lentokentän ICAO-koodi: ")
+        haku = haku.upper()
+        if haku in lentoasemat:
+            print(f"{haku}-koodia vastaava lentokenttä on {lentoasemat[haku]}.")
+        else:
+            print(f"Koodille {haku} ei löydy vastaavaa lentokenttää.")
+    else: print("Annettulle päätökselle ei ole komentoa.")
+    paatos = input(paatos_teksti)
+
+print("Lopetetaan ohjelma...\nOhjelma lopetettu.")
